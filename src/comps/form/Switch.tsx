f1d963039a37a7a2617529d3282af3ecdbdcd85d
leio99 <amicoexe@gmail.com>
2020-11-19 19:16:15 +0100

    Minor fix stili

--- src/comps/form/Switch.tsx
+++ src/comps/form/Switch.tsx
@@ -11,13 +11,20 @@ export interface IProps extends InputProps{
 export class Switch extends React.PureComponent<IProps>{
     onChange = () => this.props.onChange && this.props.onChange(this.props.value)
 
+    checkSpace = (e: any) => {
+        if(e.keyCode === 32){
+            !this.props.loading && this.onChange()
+            e.target.blur()
+        }
+    }
+
     render = (): JSX.Element => {
         const props = this.props
 
         return <div className={"dolfo-input-switch" + ((props.disabled || props.loading) ? " disabled" : "")} onClick={this.onChange}>
             <input type="checkbox" required={props.required} checked={props.checked} tabIndex={-1} />
 
-            <div className={"dolfo-switch" + (props.checked ? " checked" : "") + (props.className ? (" " + props.className) : "")}>
+            <div className={"dolfo-switch" + (props.checked ? " checked" : "") + (props.className ? (" " + props.className) : "")} tabIndex={0} onKeyUp={this.checkSpace}>
                 <div className={"dolfo-switch-dot" + (props.loading ? " loading" : "")}>
                     {props.loading && <LoadingIcon spinning />}
                 </div>
