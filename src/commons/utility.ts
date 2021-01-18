61ed03bcca2811f9008214210d60007762aa065d
leio99 <amicoexe@gmail.com>
2021-01-18 20:31:53 +0100

    Migliorata logica bisestile

--- src/commons/utility.ts
+++ src/commons/utility.ts
@@ -24,7 +24,7 @@ export const isValidDate = (d: number, m: number, y: number, h: number, i: numbe
 
 export const daysInMonth = (m: number, y: number) => {
     switch (m) {
-        case 1: return (y % 4 === 0 && y % 100) || y % 400 === 0 ? 29 : 28
+        case 1: return y % 4 === 0 || y % 100 === 0 || y % 400 === 0 ? 29 : 28
         case 8:
         case 3:
         case 5:
