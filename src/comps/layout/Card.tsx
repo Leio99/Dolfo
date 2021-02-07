d2fd3ff484e539f59bbf2d43906f8ed408982885
Leio99 <48279774+Leio99@users.noreply.github.com>
2021-02-07 19:01:29 +0100

    Fix card

--- src/comps/layout/Card.tsx
+++ src/comps/layout/Card.tsx
@@ -1,18 +1,33 @@
 import React, { CSSProperties } from "react"
 
 export interface IProps{
-    readonly title?: string | JSX.Element
     readonly className?: string
     readonly style?: CSSProperties
 }
+export interface CardProps extends IProps{
+    readonly title?: string | JSX.Element
+}
 
-export class Card extends React.PureComponent<IProps>{
+export class Card extends React.PureComponent<CardProps>{
     render = (): JSX.Element => {
         const props = this.props
 
         return <div className={"dolfo-card" + (props.className ? (" " + props.className) : "")} style={props.style}>
             {props.title && <div className="dolfo-card-title">{props.title}</div>}
             {props.children}
+        </div>
+    }
+}
+
+export class CardActions extends React.PureComponent<IProps>{
+    render = (): JSX.Element => {
+        const props = this.props
+
+        return <div className="dolfo-card-actions-inner">
+            <br />
+            <div className={"dolfo-card-actions" + (props.className ? (" " + props.className) : "")} style={props.style}>
+                {props.children}
+            </div>
         </div>
     }
 }
