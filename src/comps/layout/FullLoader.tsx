0fa3da0d20538a3a8c17b3bf771ee90ffa0f9b85
Leio99 <48279774+Leio99@users.noreply.github.com>
2021-02-20 16:27:20 +0100

    fix fullloader

--- src/comps/layout/FullLoader.tsx
+++ src/comps/layout/FullLoader.tsx
@@ -16,14 +16,14 @@ export class FullLoader extends React.Component{
 
         ReactDOM.render(<div className="dolfo-full-loader-inner">
             {
-                data.type === "balls" ? <div className="balls-loading">
+                data?.type === "balls" ? <div className="balls-loading">
                     <div className="ball-inner"></div>
                 </div> : <div className="circle-loading">
                     <div className="circle-loading-inner"></div>
                 </div>
             }
 
-            {data.loadingText && <span className="loading-text">{data.loadingText}</span>}
+            {data?.loadingText && <span className="loading-text">{data.loadingText}</span>}
         </div>, loader)
 
         return loader as { close?: () => void }
