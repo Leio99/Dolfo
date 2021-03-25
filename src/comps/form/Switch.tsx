c22c9f66fb3e5e153dda4fe3c778d549325b368f
Leio99 <48279774+Leio99@users.noreply.github.com>
2021-03-25 21:12:51 +0100

    Update Switch.tsx

--- src/comps/form/Switch.tsx
+++ src/comps/form/Switch.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { InputProps } from "../shared/models/InputProps"
 import { LoadingIcon } from "../layout/Icon"
+import { Constants } from "../shared/Constants"
 
 export interface IProps extends InputProps{
     readonly value?: any
@@ -8,8 +9,32 @@ export interface IProps extends InputProps{
     readonly loading?: boolean
 }
 
-export class Switch extends React.PureComponent<IProps>{
-    onChange = () => this.props.onChange && this.props.onChange(this.props.value)
+export class Switch extends React.PureComponent<IProps, IProps>{
+    constructor(props: IProps){
+        super(props)
+        
+        this.state = {
+            checked: props.checked || false
+        }
+    }
+
+    componentDidUpdate = (prevProps: IProps) => {
+        if(prevProps.checked !== this.props.checked){
+            this.setState({
+                checked: this.props.checked
+            })
+        }
+    }
+
+    onChange = () => {
+        if(this.props.disabled) return
+        
+        !this.props.onChange && this.setState({
+            checked: !this.state.checked
+        })
+
+        this.props.onChange && this.props.onChange(this.props.value)
+    }
 
     checkSpace = (e: any) => {
         if(e.key.charCodeAt(0) === 32){
@@ -21,18 +46,22 @@ export class Switch extends React.PureComponent<IProps>{
     }
 
     render = (): JSX.Element => {
-        const props = this.props
+        const props = this.props,
+        { checked } = this.state
 
         return <div className={"dolfo-input-switch" + ((props.disabled || props.loading) ? " disabled" : "") + (props.className ? (" " + props.className) : "")} onClick={this.onChange} style={props.wrapperStyle}>
-            <input type="checkbox" required={props.required} checked={props.checked} tabIndex={-1} />
+            <input type="checkbox" required={props.required} checked={checked} tabIndex={-1} />
 
-            <div className={"dolfo-switch" + (props.checked ? " checked" : "")} tabIndex={props.loading ? -1 : 0} onKeyUp={this.checkSpace} style={props.style}>
+            <div className={"dolfo-switch" + (checked ? " checked" : "")} tabIndex={props.loading ? -1 : 0} onKeyUp={this.checkSpace} style={props.style}>
                 <div className={"dolfo-switch-dot" + (props.loading ? " loading" : "")}>
                     {props.loading && <LoadingIcon spinning />}
                 </div>
             </div>
             
-            <label>{props.label}</label>
+            {props.label && <label>
+                {props.label}
+                {props.required && <span className="dolfo-input-required" data-tooltip={Constants.REQUIRED_FIELD}> *</span>} 
+            </label>}
         </div>
     }
 }
