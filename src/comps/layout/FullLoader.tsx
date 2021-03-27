dfd98212eaad333f396a7a70fe892f23c131c534
Leio99 <48279774+Leio99@users.noreply.github.com>
2021-03-27 21:46:29 +0100

    Update FullLoader.tsx

--- src/comps/layout/FullLoader.tsx
+++ src/comps/layout/FullLoader.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom"
 
 export class FullLoader extends React.Component{
     static show = (data?: { loadingText?: string, type?: "circle" | "balls" }) => {
