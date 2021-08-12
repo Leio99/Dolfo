8bf053cc9817538f26c852af93e5e09a089f3711
Leio99 <amicoexe@gmail.com>
2021-08-12 09:44:32 +0200

    Fix timeline

--- src/comps/layout/Timeline.tsx
+++ src/comps/layout/Timeline.tsx
@@ -4,16 +4,20 @@ interface TimelineItemProps{
     readonly position?: "left" | "right"
     readonly style?: CSSProperties
     readonly pinColor?: string
+    readonly hideEmptySpace?: boolean
 }
 
 export class Timeline extends React.Component{
     getOptions = (): TimelineItem[] => React.Children.map(this.props.children, (child: any) => child).filter(o => !!o)
 
     render = (): JSX.Element => {
-        const options = this.getOptions()
+        const options = this.getOptions(),
+        hideAll = options.every(o => !o.props.position || o.props.position === (options[0].props.position || "left"))
+
+        console.warn(hideAll)
 
         return <div className="dolfo-timeline">
-            {options}
+            {options.map(o => <TimelineItem {...o.props} hideEmptySpace={hideAll} />)}
         </div>
     }
 }
@@ -25,13 +29,13 @@ export class TimelineItem extends React.Component<TimelineItemProps>{
             {props.children}
         </div>
 
-        return <div className={"dolfo-timeline-item-row " + (props.position || "left")}>
+        return <div className={"dolfo-timeline-item-row " + (props.position || "left") + (props.hideEmptySpace ? " hide-empty" : "")}>
             {
-                props.position !== "right" ? item : <div className="dolfo-timeline-empty"></div>
+                props.position !== "right" ? item : !props.hideEmptySpace ? <div className="dolfo-timeline-empty"></div> : null
             }
             <div className="dolfo-timeline-sign" style={{ color: props.pinColor }}></div>
             {
-                props.position === "right" ? item : <div className="dolfo-timeline-empty"></div>
+                props.position === "right" ? item : !props.hideEmptySpace ? <div className="dolfo-timeline-empty"></div> : null
             }
         </div>
     }
