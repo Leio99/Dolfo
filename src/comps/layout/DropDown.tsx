69220b7d556d6716a7c87c21f8793f1c4d53bb05
Leio99 <amicoexe@gmail.com>
2022-01-24 15:32:13 +0100

    fix dropdown

--- src/comps/layout/DropDown.tsx
+++ src/comps/layout/DropDown.tsx
@@ -1,4 +1,5 @@
 import React, { CSSProperties } from "react"
+import ReactDOM from "react-dom"
 import { Icon } from "./Icon"
 
 interface IProps{
@@ -31,7 +32,43 @@ export class DropDown extends React.Component<IProps, IState>{
 
     close = () => this.setState({ opened: false })
     
-    open = () => !this.props.disabled && this.setState({ opened: true })
+    open = () => {
+        if(this.props.disabled)
+            return
+
+        this.setState({ opened: true }, () => {
+            const node = ReactDOM.findDOMNode(this) as HTMLElement,
+            container = node.querySelector(".dropdown-items-container") as HTMLElement,
+            items = node.querySelectorAll(".dropdown-items-container .dolfo-dropdown-item")
+            let maxWidth = 0
+
+            items.forEach(i => {
+                const span = document.createElement("span"),
+                label = node.querySelector(".dolfo-dropdown-label"),
+                pL = window.getComputedStyle(label, null).getPropertyValue("padding-left"),
+                pR = window.getComputedStyle(label, null).getPropertyValue("padding-right")
+
+                span.innerHTML = i.innerHTML
+                span.style.position = "fixed"
+                span.style.opacity = "0"
+                span.style.pointerEvents = "none"
+                span.style.paddingLeft = pL
+                span.style.paddingRight = pR
+
+                document.body.append(span)
+
+                const width = span.clientWidth
+
+                span.remove()
+
+                if(width > maxWidth)
+                    maxWidth = width
+            })
+
+            if(node.clientWidth < maxWidth)
+                container.style.width = maxWidth + "px"
+        })
+    }
 
     getItems = (): DropDownItem[] => React.Children.map(this.props.children, (child: any) => child).filter(o => !!o)
 
