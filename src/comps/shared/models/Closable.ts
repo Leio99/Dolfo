15fc5342d90ed6be711956c4d766dbaf0a94cbcd
Leonardo Grandolfo <leonardo.grandolfo@maggioli.it>
2022-05-20 11:51:26 +0200

    update closable

--- src/comps/shared/models/Closable.ts
+++ src/comps/shared/models/Closable.ts
@@ -1,11 +1,19 @@
 export class Closable{
     private readonly _close: () => void
+    private _isClosed = false
 
-    constructor(closeFn: () => void){
-        this._close = closeFn
+    constructor(closeFn?: () => void){
+        this._close = () => {
+            closeFn && closeFn()
+            this._isClosed = true
+        }
     }
 
     get close(){
         return this._close
+    }
+
+    get isClosed(){
+        return this._isClosed
     }
 }
