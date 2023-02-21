0dc3826e2872c6d6e3405231b6f885c07be40026
Leio99 <amicoexe@gmail.com>
2023-02-21 21:44:59 +0100

    Revert

--- src/comps/form/CheckBox.tsx
+++ src/comps/form/CheckBox.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react"
+import React from "react"
 import { BaseInputProps } from "../shared/models/InputProps"
 import { CheckIcon } from "../layout/Icon"
 import { Constants } from "../shared/Constants"
@@ -8,45 +8,52 @@ export interface CheckBoxProps extends BaseInputProps{
     readonly checked?: boolean
 }
 
-export const CheckBox = (props: CheckBoxProps) => {
-    const [checked, setChecked] = useState(props.checked || false),
-    onChange = () => {
-        if(props.disabled) return
+export class CheckBox extends React.PureComponent<CheckBoxProps, CheckBoxProps>{ 
+    constructor(props: CheckBoxProps){
+        super(props)
         
-        setChecked(!checked)
+        this.state = {
+            checked: props.checked || false
+        }
+    }
+
+    componentDidUpdate = (prevProps: CheckBoxProps): void => {
+        if(prevProps.checked !== this.props.checked || (this.props.checked !== this.state.checked && this.props.checked !== prevProps.checked))
+            this.setState({ checked: !!this.props.checked })
+    }
+
+    onChange = (): void => {
+        if(this.props.disabled) return
         
-        if(props.onChange)
-            props.onChange(!checked)
-    },
-    checkSpace = (e: React.KeyboardEvent<HTMLDivElement>): void => {
+        this.setState({
+            checked: !this.state.checked
+        }, () => this.props.onChange && this.props.onChange(this.state.checked))
+    }
+
+    checkSpace = (e: any): void => {
         if(e.key.charCodeAt(0) === 32){
-            onChange()
+            this.onChange()
             e.preventDefault()
             e.stopPropagation()
         }
-    },
-    checkedRef = useRef<boolean>()
+    }
 
-    useEffect(() => {
-        if(checkedRef.current !== props.checked)
-            setChecked(!!props.checked)
-    }, [props.checked])
+    render = (): JSX.Element => {
+        const { props } = this,
+        { checked } = this.state
 
-    useEffect(() => {
-        checkedRef.current = checked
-    }, [checked])
+        return <div className={"dolfo-checkbox" + (props.className ? (" " + props.className) : "") + (props.disabled ? " disabled" : "")} style={props.style} onClick={this.onChange}>
+            <input type="checkbox" required={props.required} checked={checked} tabIndex={-1} readOnly />
 
-    return <div className={"dolfo-checkbox" + (props.className ? (" " + props.className) : "") + (props.disabled ? " disabled" : "")} style={props.style} onClick={onChange}>
-        <input type="checkbox" required={props.required} checked={checked} tabIndex={-1} readOnly />
-
-        <div className={"dolfo-checkbox-square" + (checked ? " checked" : "")} tabIndex={0} onKeyUp={checkSpace}>
-            <CheckIcon />
+            <div className={"dolfo-checkbox-square" + (checked ? " checked" : "")} tabIndex={0} onKeyUp={this.checkSpace}>
+                <CheckIcon />
+            </div>
+            {props.label && <label className="dolfo-checkbox-label">
+                {props.label}
+                {props.required && <Tooltip tooltip={Constants.REQUIRED_FIELD}>
+                    <span className="dolfo-input-required"> *</span>
+                </Tooltip>} 
+            </label>}
         </div>
-        {props.label && <label className="dolfo-checkbox-label">
-            {props.label}
-            {props.required && <Tooltip tooltip={Constants.REQUIRED_FIELD}>
-                <span className="dolfo-input-required"> *</span>
-            </Tooltip>} 
-        </label>}
-    </div>
+    }
 }
