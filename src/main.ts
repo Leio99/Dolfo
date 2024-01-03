9529ca6e2a140d4b4b5c913bb57627798edb1d8f
Leio99 <amicoexe@gmail.com>
2024-01-03 21:42:29 +0100

    Esportato tutto dal main

--- src/main.ts
+++ src/main.ts
@@ -1,113 +1,65 @@
-import { Autocomplete } from "./comps/form/Autocomplete"
-import { CheckBox } from "./comps/form/CheckBox"
-import DatePicker from "./comps/form/DatePicker"
-import { Option } from "./comps/form/Option"
-import { PinInput } from "./comps/form/PinInput"
-import { RadioButton } from "./comps/form/RadioButton"
-import Select from "./comps/form/Select"
-import { SelectableCardList } from "./comps/form/SelectableCardList"
-import { Switch } from "./comps/form/Switch"
-import { TextInput } from "./comps/form/TextInput"
-import TimePicker from "./comps/form/TimePicker"
-import { TransferList } from "./comps/form/TransferList"
-import { Uploader } from "./comps/form/Uploader"
-import { Accordion } from "./comps/layout/Accordion"
-import { Alert } from "./comps/layout/Alert"
-import { Avatar } from "./comps/layout/Avatar"
-import { BreadCrumb } from "./comps/layout/BreadCrumb"
-import Button from "./comps/layout/Button"
-import { Calendar } from "./comps/layout/Calendar"
-import { Card } from "./comps/layout/Card"
-import { CardsSwiper, SwiperCard } from "./comps/layout/CardsSwiper"
-import { ContextMenu } from "./comps/layout/ContextMenu"
-import { Dialog } from "./comps/layout/Dialog"
-import { Display } from "./comps/layout/Display"
-import { DropDown } from "./comps/layout/DropDown"
-import { FullLoader } from "./comps/layout/FullLoader"
-import { Header } from "./comps/layout/Header"
-import { HorizontalDisplayer, HorizontalDisplayItem } from "./comps/layout/HorizontalDisplayer"
-import { Icon } from "./comps/layout/Icon"
-import { Label } from "./comps/layout/Label"
-import { ListSideMenu } from "./comps/layout/ListSideMenu"
-import { MessageBox } from "./comps/layout/MessageBox"
-import { NotificationMsg } from "./comps/layout/NotificationMsg"
-import { Pagination } from "./comps/layout/Pagination"
-import { Popover } from "./comps/layout/Popover"
-import { Progress } from "./comps/layout/Progress"
-import { SideMenu, MenuItem, SubMenu } from "./comps/layout/SideMenu"
-import { SlideShow } from "./comps/layout/SlideShow"
-import { Spotlight } from "./comps/layout/Spotlight"
-import { Status } from "./comps/layout/Status"
-import { Stepper } from "./comps/layout/Stepper"
-import { Swiper } from "./comps/layout/Swiper"
-import { CardTable } from "./comps/layout/table/CardTable"
-import { MasterDetail } from "./comps/layout/table/MasterDetail"
-import { ResultsView } from "./comps/layout/table/ResultsView"
-import { Table } from "./comps/layout/table/Table"
-import { TreeView } from "./comps/layout/table/TreeView"
-import { Tabs, Tab } from "./comps/layout/Tabs"
-import { Timeline } from "./comps/layout/Timeline"
-import { Tooltip } from "./comps/layout/Tooltip"
-import { getConstant } from "./comps/shared/Constants"
+export * from "./comps/form/Autocomplete"
+export * from "./comps/form/CheckBox"
+export * from "./comps/form/DatePicker"
+export * from "./comps/form/Option"
+export * from "./comps/form/PinInput"
+export * from "./comps/form/RadioButton"
+export * from "./comps/form/Select"
+export * from "./comps/form/SelectableCardList"
+export * from "./comps/form/Switch"
+export * from "./comps/form/TextInput"
+export * from "./comps/form/TimePicker"
+export * from "./comps/form/TransferList"
+export * from "./comps/form/Uploader"
 
-export {
-    CheckBox,
-    Autocomplete,
-    DatePicker,
-    Option,
-    PinInput,
-    RadioButton,
-    Select,
-    SelectableCardList,
-    Switch,
-    TextInput,
-    TimePicker,
-    TransferList,
-    Uploader,
-    Accordion,
-    Alert,
-    Avatar,
-    BreadCrumb,
-    Button,
-    Calendar,
-    Card,
-    ContextMenu,
-    Dialog,
-    Display,
-    DropDown,
-    FullLoader,
-    Header,
-    Icon,
-    Label,
-    ListSideMenu,
-    MenuItem,
-    MessageBox,
-    NotificationMsg,
-    Pagination,
-    Popover,
-    Progress,
-    SideMenu,
-    SlideShow,
-    Spotlight,
-    Status,
-    Stepper,
-    SubMenu,
-    Swiper,
-    Tabs, Tab,
-    Timeline,
-    Tooltip,
-    getConstant,
-    Table,
-    CardTable,
-    MasterDetail,
-    TreeView,
-    ResultsView,
-    HorizontalDisplayer,
-    HorizontalDisplayItem,
-    CardsSwiper,
-    SwiperCard
-}
+export * from "./comps/layout/Accordion"
+export * from "./comps/layout/Alert"
+export * from "./comps/layout/Avatar"
+export * from "./comps/layout/BreadCrumb"
+export * from "./comps/layout/Button"
+export * from "./comps/shared/models/IconModel"
+export * from "./comps/layout/Calendar"
+export * from "./comps/layout/Card"
+export * from "./comps/layout/CardsSwiper"
+export * from "./comps/layout/ContextMenu"
+export * from "./comps/layout/Dialog"
+export * from "./comps/layout/Display"
+export * from "./comps/layout/DropDown"
+export * from "./comps/layout/FullLoader"
+export * from "./comps/layout/Header"
+export * from "./comps/layout/HorizontalDisplayer"
+export * from "./comps/layout/Icon"
+export * from "./comps/layout/Label"
+export * from "./comps/layout/ListSideMenu"
+export * from "./comps/layout/MessageBox"
+export * from "./comps/layout/Pagination"
+export * from "./comps/layout/Popover"
+export * from "./comps/layout/Progress"
+export * from "./comps/layout/SideMenu"
+export * from "./comps/layout/SlideShow"
+export * from "./comps/layout/Spotlight"
+export * from "./comps/layout/Status"
+export * from "./comps/layout/Stepper"
+export * from "./comps/layout/Swiper"
+export * from "./comps/layout/table/CardTable"
+export * from "./comps/layout/table/MasterDetail"
+export * from "./comps/layout/table/ResultsView"
+export * from "./comps/layout/table/Table"
+export * from "./comps/layout/table/TreeView"
+export * from "./comps/layout/Tabs"
+export * from "./comps/layout/Timeline"
+export * from "./comps/layout/Tooltip"
+export * from "./comps/layout/table/ResultsView"
+export * from "./comps/layout/NotificationMsg"
 
 export * from "./comps/shared/utility"
-export type { IColumn, IDataColumn } from "./comps/shared/models/IColumn"
-export type { ViewType } from "./comps/layout/table/ResultsView"
+export * from "./comps/shared/Constants"
+export * from "./comps/shared/models/IColumn"
+export * from "./comps/shared/models/EventManager"
+
+import Button from "./comps/layout/Button"
+import DatePicker from "./comps/form/DatePicker"
+import TimePicker from "./comps/form/TimePicker"
+import Select from "./comps/form/Select"
+
+export { Button, DatePicker, TimePicker, Select }
